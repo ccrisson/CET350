75586e4f3043c8175c8f998afb86ef7bfc9aaeaa
Christopher <crisson.christopher@gmail.com>
2019-02-17 19:57:16 -0500

    GUI is rough but working. ActionListener started

--- Program3/Main.java
+++ Program3/Main.java
@@ -3,34 +3,93 @@ import java.io.*;
 import java.util.*;
 import java.lang.*;
 import java.awt.*;
+import java.awt.List;
 import java.awt.event.*;
 
-class Main extends Frame implements WindowListener{
-	Main(){
-		// Hold the origin location for the frame
-		int locX = 200;
-		int locY = 200;
-		int height = 500;
-		int width = 500;
+class Main extends Frame implements ActionListener, WindowListener{
+	String path = System.getProperty("user.dir");
 
-		// Create frame elements
-		Button OKButton = new Button(" OK ");
-		OKButton.setSize(50,25);
-		OKButton.setLocation(locX + 50, locY + 50);
+	List fileView = new List();
+	Label sourceLabel;
+	public Main(){
+		// Hold the origin location for the frame
+		// Set Frame size
+		//int locX = 200;
+		//int locY = 200;
+		super();
+		int height = 800;
+		int width = 300;
+
+	
+		
+		File f = new File(path);
+		ArrayList<String> fileNames = new ArrayList<String>(Arrays.asList(f.list()));
 
 		// Set default frame properties
-		this.setLayout(null);
-		this.setVisible(true);
-		this.setLocation(locX, locY);
-		this.setSize(height, width);
-		this.setTitle("Test Window");
-		this.setBackground(Color.green); 
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.anchor = GridBagConstraints.WEST;
+
+		this.setTitle(path);
 		addWindowListener(this);
 
+		// Create frame elements
+		
+		
+		try{
+			if (f.getParentFile().exists()) {
+				fileView.add("..");
+			} 
+		}catch (NullPointerException e){
+			// Folder is root	
+		}
+		
+		for(int i = 0; i < fileNames.size(); i++){
+			fileView.add(fileNames.get(i));
+		}
+		gbc.weightx = 1.0;
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.fill = GridBagConstraints.BOTH;
+		fileView.addActionListener(this);
+		this.add(fileView, gbc);
+		//this.fileView = fileView;
+
+		sourceLabel = new Label("Source: " + path);
+		gbc.weightx = 0.0;
+		gbc.fill = GridBagConstraints.NONE;
+		//sourceLabel.addActionListener(this);
+		this.add(sourceLabel, gbc);
+		
+		Button targetButton = new Button("Target: ");
+		gbc.gridwidth = GridBagConstraints.RELATIVE;
+		this.add(targetButton, gbc);
+		
+		Label targetLabel = new Label(" target label ");
+		gbc.gridwidth = GridBagConstraints.NONE;
+		this.add(targetLabel,gbc);
+
+		Label fileLabel = new Label("Filename: ");
+		gbc.gridwidth = GridBagConstraints.NONE;
+		this.add(fileLabel,gbc);
+
+		TextField fileTextField = new TextField("asdfjkla;ldkfja");
+		gbc.gridwidth = GridBagConstraints.RELATIVE;
+		this.add(fileTextField,gbc);
+
+		Button OKButton = new Button(" OK ");
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		this.add(OKButton,gbc);
+	
+		this.setVisible(true);
+		this.setSize(this.getPreferredSize());
+		
+		//this.pack();
+		//fileView.setText("file view will go here");
+
 		// Add elements
-		this.add(OKButton);
-
-
+		
+		
+		
 	}
 
 	public void windowOpened(WindowEvent e){
@@ -62,8 +121,17 @@ class Main extends Frame implements WindowListener{
 
 	}
 
+	public void actionPerformed(ActionEvent e){
+		Object source = e.getSource();
+		//System.out.println(e.getSource());
+		fileView.add("something happened");
+		if(source == this.fileView){
+			sourceLabel.setText("Source: " + path + fileView.getSelectedItem());
+		}
+	}
+
 	public static void main(String[] args){
-		new Main();
+		Main m = new Main();
 
 		/* Without extending frame
 		Frame f =  new Frame();
